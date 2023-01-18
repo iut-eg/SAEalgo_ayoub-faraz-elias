b8314b0e86eb50dccb9914078d18e48cff3ca38b
iut-eg (laptop) <eguebasi@iut.univ-pparis8.fr>
2023-01-18 14:19:41 +0100

    triPeigne.c seems working, i guess...

--- src/triPeigne-cb.c
+++ src/triPeigne-cb.c
@@ -3,53 +3,65 @@
 #include <stdbool.h>
 #include <time.h>
 void randomArray(int *array, int n);
+void triPeigne(int *arr, int n);
+void printArray(int *arr, int n);
 
 int main(){
-    int n = 100000;
+    int n = 1000000 ;
 
 
    int array[n];
-   printf("The array has %zu elements\n", sizeof array / sizeof *array);
 
    randomArray(array, n);
-    /*
-    time_t seconds;
-    time_t temps;
 
-    temps = time(NULL);
-    array = triPeigne(array);
-    seconds = time(NULL) - temps;
-    printf("tri peigne: %ld seconds\n", seconds);
-    */
+    double seconds;
+    double temps1, temps2;
+
+    temps1 = clock();
+    printf("\ntemps1 = %lf \n", temps1);
+    triPeigne(array, n);
+
+    temps2 = clock();
+    printf("temps2 = %lf \n\n", temps2);
+    seconds = (double)(temps2 - temps1) / CLOCKS_PER_SEC;
+
+    //printArray(array, n);
+    printf("\n tri peigne: %lf seconds\n", seconds);
+
     return 0;
 }
 
 
-void randomArray(int *array, int n) {
+void randomArray(int *arr, int n) {
 
 
-        for (int i = 0 ; i < n ; i++){
-            array[i] = rand(); // tirage aléatoire
-            printf("array[%d] = %d \n", i, array[i]);
+        for (int i = 0 ; i < n ; i++)
+        {
+            arr[i] = rand(); // tirage aléatoire
+            //printf("array[%d] = %d \n", i, arr[i]);
         }
 
-}/*
-int* triPeigne(int arr[]) {
+}
+void triPeigne(int *arr, int n) {
 
-        int arrlength = arrsizeof(arr) / sizeof(arr[0]);
+        int arrlength = n;
         int ecart = arrlength;
         double shrink = 1.3; // Offre les performances optimales
         bool echange = true;
 
-        while (ecart > 1 || echange) {
-            if (ecart > 1) {
+        while (ecart > 1 || echange)
+        {
+            if (ecart > 1)
+            {
                 ecart = (int) (ecart / shrink);
             }
 
             int i = 0;
             echange = false;
-            while (i + ecart < arrlength) {
-                if (arr[i] > arr[i + ecart]) {
+            while (i + ecart < arrlength)
+            {
+                if (arr[i] > arr[i + ecart])
+                {
                     int temp = arr[i];
                     arr[i] = arr[i + ecart];
                     arr[i + ecart] = temp;
@@ -59,9 +71,16 @@ int* triPeigne(int arr[]) {
             }
 
         }
-        return arr;
 }
 
-*/
+void printArray(int *arr, int n) {
+        for (int i = 0 ; i < n ; i++){
+            printf("array[%d] = %d \n", i, arr[i]);
+        }
+
+}
+
+
+
 
 
